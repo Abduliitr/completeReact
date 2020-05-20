0e1dfc5dd972e56d2e58f898e0ae5408ec4a53e6
Abduliitr <abdul.thomso@gmail.com>
2020-05-20 06:36:32 +0530

    components part 2 | state lifting, etc

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import { Navbar, NavbarBrand } from 'reactstrap';
+import { Navbar, NavbarBrand, Form } from 'reactstrap';
 import Menu from './components/MenuComponent';
 import './App.css';
+import {DISHES} from './shared/dishes';
 
 class App extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      dishes: DISHES
+    };
+  }
   render() { 
     return ( 
       <div className="App">
@@ -13,7 +22,7 @@ class App extends Component {
             <NavbarBrand href="/">Ristorante con Fusion</NavbarBrand>
           </div>
         </Navbar>
-        <Menu />
+        <Menu dishes={this.state.dishes}/>
       </div>
     );
   }
